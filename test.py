75937f0a7f540fce5f35bfb8d65f36b3d298ee65
Ayush1ranjan-admin <105307301+Ayush1ranjan-admin@users.noreply.github.com>
2025-05-30 00:27:50 +0530

    commit 1b2d3f4g5h6i7j8k9l0m

---
+++ test.py
@@ -0,0 +1,15 @@
+import mysql.connector as m
+mydb = m.connect(
+  host="localhost",
+  user="root",
+  password="swordsaint"
+)
+cur = mydb.cursor()
+cur.execute("create database testdb")
+cur.execute("use testdb")
+cur.execute("create table testtable (id int, name varchar(255))") 
+cur.execute("insert into testtable (id, name) values (1, 'test')")
+cur.execute("select * from testtable")
+for row in cur.fetchall():
+    print(row)
+mydb.commit()
